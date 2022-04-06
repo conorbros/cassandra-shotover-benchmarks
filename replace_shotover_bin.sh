3c9ac6a5b07f573fd2d708d792c41464c8bd9265
conorbros <conor.brosnan@instaclustr.com>
2022-04-07 01:31:16 +1000

    add some more scripts

---
+++ replace_shotover_bin.sh
@@ -0,0 +1,3 @@
+sudo mv shotover-proxy/shotover-proxy/release/shotover-proxy /usr/bin/shotover-proxy
+sudo rm -r shotover-proxy
+sudo systemctl restart shotover
--- setup_perf_tools.sh
+++ setup_perf_tools.sh
@@ -1,7 +1,22 @@
+# install deps
+
 sudo yum update -y
 sudo yum install git openssl-devel openssl perf -y
 sudo yum groupinstall "Development Tools" -y
+
+# install rust and rust deps
+
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
 cargo install inferno
-git clone https://github.com/shotover/shotover-proxy
+
+# install shotover
+git clone https://github.com/conorbros/shotover-proxy
+git checkout --branch benches
+cd shotover-proxy
+
+
+# build shotover and replace existing binary
+
+cargo build --release
+cd ..
