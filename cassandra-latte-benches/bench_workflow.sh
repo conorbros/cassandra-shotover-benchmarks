ad7a8c10663f5c12dbea6267de0db128b8b1aea0
conorbros <conor.brosnan@instaclustr.com>
2022-04-19 12:20:07 +1000

    change the benches workflow

--- cassandra-latte-benches/bench_workflow.sh
+++ cassandra-latte-benches/bench_workflow.sh
@@ -1,31 +1,16 @@
 ADDRESS="${1}"
 
-./run_benches.sh $ADDRESS 200 600s 1
-./run_benches.sh $ADDRESS 500 600s 1
-./run_benches.sh $ADDRESS 1000 600s 1
-./run_benches.sh $ADDRESS 5000 600s 1
+./run_benches.sh $ADDRESS 200 120s 1
+./run_benches.sh $ADDRESS 500 120s 1
+./run_benches.sh $ADDRESS 1000 120s 1
+./run_benches.sh $ADDRESS 5000 120s 1
 
-./run_benches.sh $ADDRESS 200 600s 16
-./run_benches.sh $ADDRESS 500 600s 16
-./run_benches.sh $ADDRESS 1000 600s 16
-./run_benches.sh $ADDRESS 5000 600s 16
+./run_benches.sh $ADDRESS 200 120s 64
+./run_benches.sh $ADDRESS 500 120s 64
+./run_benches.sh $ADDRESS 1000 120s 64
+./run_benches.sh $ADDRESS 5000 120s 64
 
-./run_benches.sh $ADDRESS 200 600s 32
-./run_benches.sh $ADDRESS 500 600s 32
-./run_benches.sh $ADDRESS 1000 600s 32
-./run_benches.sh $ADDRESS 5000 600s 32
-
-./run_benches.sh $ADDRESS 200 600s 64
-./run_benches.sh $ADDRESS 500 600s 64
-./run_benches.sh $ADDRESS 1000 600s 64
-./run_benches.sh $ADDRESS 5000 600s 64
-
-./run_benches.sh $ADDRESS 200 600s 128
-./run_benches.sh $ADDRESS 500 600s 128
-./run_benches.sh $ADDRESS 1000 600s 128
-./run_benches.sh $ADDRESS 5000 600s 128
-
-./run_benches.sh $ADDRESS 200 600s 256
-./run_benches.sh $ADDRESS 500 600s 256
-./run_benches.sh $ADDRESS 1000 600s 256
-./run_benches.sh $ADDRESS 5000 600s 256
+./run_benches.sh $ADDRESS 200 120s 128
+./run_benches.sh $ADDRESS 500 120s 128
+./run_benches.sh $ADDRESS 1000 120s 128
+./run_benches.sh $ADDRESS 5000 120s 128
