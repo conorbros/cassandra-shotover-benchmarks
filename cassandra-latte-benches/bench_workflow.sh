9d9055c46e9d04a097c9d7fae052d04ca59493a0
conorbros <conor.brosnan@instaclustr.com>
2022-04-19 16:14:01 +1000

    update bench

--- cassandra-latte-benches/bench_workflow.sh
+++ cassandra-latte-benches/bench_workflow.sh
@@ -4,13 +4,12 @@ ADDRESS="${1}"
 ./run_benches.sh $ADDRESS 500 120s 1
 ./run_benches.sh $ADDRESS 1000 120s 1
 ./run_benches.sh $ADDRESS 5000 120s 1
-
-./run_benches.sh $ADDRESS 200 120s 64
-./run_benches.sh $ADDRESS 500 120s 64
-./run_benches.sh $ADDRESS 1000 120s 64
-./run_benches.sh $ADDRESS 5000 120s 64
+./run_benches.sh $ADDRESS 10000 120s 1
+./run_benches.sh $ADDRESS 20000 120s 1
 
 ./run_benches.sh $ADDRESS 200 120s 128
 ./run_benches.sh $ADDRESS 500 120s 128
 ./run_benches.sh $ADDRESS 1000 120s 128
 ./run_benches.sh $ADDRESS 5000 120s 128
+./run_benches.sh $ADDRESS 10000 120s 128
+./run_benches.sh $ADDRESS 20000 120s 128
