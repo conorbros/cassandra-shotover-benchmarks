f8f9f88c085c8ac963812e3da3a4380736220acb
conorbros <conor.brosnan@instaclustr.com>
2022-05-17 18:42:15 +1000

    update topology

--- cassandra-latte-benches/latte.sh
+++ cassandra-latte-benches/latte.sh
@@ -4,7 +4,8 @@ set -e
 
 ADDRESS=${1}
 CONNECTIONS=${2}
+RATE=${3}
 
 latte schema --user cassandra --password cassandra read.rn -- ${ADDRESS}
 latte load --user cassandra --password cassandra read.rn -- ${ADDRESS}
-latte run --user cassandra --password cassandra read.rn --connections ${CONNECTIONS} -- ${ADDRESS}
+latte run --user cassandra --password cassandra read.rn --connections ${CONNECTIONS} --rate ${RATE} -- ${ADDRESS}
